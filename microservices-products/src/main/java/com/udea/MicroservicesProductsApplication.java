e293dace870a2967474033de160c571732c3a2d3
aperezmm <33065509+aperezmm@users.noreply.github.com>
2018-10-29 21:56:58 -0500

    Microservices products

---
+++ microservices-products/src/main/java/com/udea/MicroservicesProductsApplication.java
@@ -0,0 +1,12 @@
+package com.udea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProductsApplication.class, args);
+	}
+}
